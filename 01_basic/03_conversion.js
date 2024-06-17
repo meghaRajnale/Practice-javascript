19030a7d3735ba788a31ef00a4b89d6ad1e57e8f
meghaRajnale <167089453+meghaRajnale@users.noreply.github.com>
2024-06-17 06:14:41 +0000

    conversion of data type

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,24 @@
+let score = "falsedfs"
+
+
+console.log(typeof score);
+console.log(typeof(score));//() mein var likhna matlab like a method 
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1; False => 0
+
+let isLoggedIn = "abc"
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true, 0=>false
+//" " => false
+//"megha"=>true
+
