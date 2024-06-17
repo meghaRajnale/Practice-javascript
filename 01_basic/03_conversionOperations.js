4496264cccff2f6f8a9d9c3876e2c4c6e54e4986
meghaRajnale <167089453+meghaRajnale@users.noreply.github.com>
2024-06-17 07:30:32 +0000

    comaprision operator

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,66 @@
+let score = "falsedfs"
+
+
+console.log(typeof score);
+console.log(typeof(score));//() mein var likhna matlab like a method 
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1; False => 0
+
+let isLoggedIn = "abc"
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true, 0=>false
+//" " => false
+//"megha"=>true
+
+//***************************************************Operatoration**********************************************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " megha"
+let str3= str1 + str2;
+console.log(str3);// hello megha
+
+console.log("1" + 2);//12
+console.log( 1 + "2");//12
+console.log("1" + 2 + 2 );//122
+console.log(1 + 2 + "2" );//32
+
+console.log(+true);//1
+// console.log(true+);//syntax error
+
+console.log(+" ");//0
+
+
+//increment operator
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+
+//search- prefix and postfix js mdn
+//link to study- ++++++++++++++
+
+
+
+
+
